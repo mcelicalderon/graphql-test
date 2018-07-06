3017341714089811329f9aaa0e522154dc1e6570
Mario Celi <mcelicalderon@gmail.com>
2018-07-06 13:41:16 -0500

    Add user computed property

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,4 +5,17 @@ class Types::UserType < Types::BaseObject
   field :last_name, String, null: true
   field :email, String, null: false
   field :birthday, Types::DateType, null: true
+  field :test, String, null: true
+  field :age, Integer, null: true
+
+  def test
+    'TEST STRING HERE!'
+  end
+
+  def age
+    return if birthday.blank?
+    temp = Time.zone.today.year - birthday.year
+    temp -= 1 if Time.zone.today < birthday + temp.years
+    temp
+  end
 end
